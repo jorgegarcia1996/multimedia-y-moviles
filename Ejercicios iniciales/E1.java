eb0a3431341f212c8a2449440d5d7d6a281f1b8e
jorgegarcia1996 <jorgetrapiche1996@gmail.com>
2019-09-23 09:11:21 +0200

    Ejercicios iniciales

---
+++ Ejercicios iniciales/E1.java
@@ -0,0 +1,25 @@
+
+package pruebas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JorgeGarciaMolina
+ */
+public class E1 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    //Pedir la frase
+    System.out.println("Comprobador de palíndromos.");
+    System.out.println("Introduzca la palabra o frase a comprobar sin espacios:");
+    String frase = new String(s.nextLine());
+    
+    //Invertir la frase
+    
+    
+    
+  }
+  
+}
---
+++ Ejercicios iniciales/E2.java
@@ -0,0 +1,15 @@
+package pruebas;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author JorgeGarciaMolina
+ */
+public class E2 {
+  public static void main(String[] args) {
+    Scanner s = new Scanner(System.in);
+    
+    
+  }
+}
